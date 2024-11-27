b9e4824211a4ec741ebf19df111e28ab4194de42
Yunji <wendyunji@gmail.com>
2024-11-27 11:49:59 +0900

    solved

--- bfs/[BOJ]_2178.py
+++ bfs/[BOJ]_2178.py
@@ -1,38 +1,40 @@
 from collections import deque
-
+import sys
+sys.setrecursionlimit(10**6)
+# ! 여기서 input = sys.stdin.readline 하면 value error 남!! 조심할것!
 n, m = map(int, input().split())
 
 graph = []
-count = 0
-for _ in range(n):
-    graph.append(list(input()))
 
-for i in range(n):
-    for j in range(m):
-        graph[i][j] = int(graph[i][j])
+for _ in range(n):
+    graph.append(list(map(int, input())))
 
 dx, dy = [1,-1,0,0], [0,0,1,-1]
 
 def bfs(x,y):
-    global count
     queue = deque([(x,y)])
 
     while queue:
-        vx, vy = queue.popleft()
-        if vx == m-1 and vy == n-1:
-            return True
-        
-        if x>=0 or y>=0 or x<m or x<n:
-            if graph[vx][vy] == 1:
-                graph[vx][vy] = 0
-                count += 1
-                print(count)
-                for i in range(4):
-                    x = vx + dx[i]
-                    y = vy + dy[i]
-                    if x>=0 or y>=0 or x<m or x<n:
-                        queue.append((x,y))
+        # 큐에서 빼기
+        x, y = queue.popleft()
+        # 4방으로 살펴보기
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            # 맵의 범위를 넘어가는 경우
+            if nx >= n or ny >= m or nx < 0 or ny < 0:
+                continue
+
+            # 벽인 경우
+            if graph[nx][ny] == 0:
+                continue
+
+            # 노드를 처음 방문하는 경우에 거리 증가
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+    
+    return graph[n-1][m-1]
 
 print(bfs(0,0))
-
-# 1의 개수는 찾아냄
