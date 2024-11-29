ac6e21b8e3913b543afd7ad71bae6e9305f893de
Yunji <wendyunji@gmail.com>
2024-11-29 18:12:32 +0900

    solved
    빨리 푼다고 어이없는 실수 주의!!

--- bfs/[BOJ]_2583.py
+++ bfs/[BOJ]_2583.py
@@ -9,7 +9,7 @@ for _ in range(k):
     # 어디에 저장?
     for i in range(sx, ex):
         for j in range(sy, ey):
-            graph[i][j] = -1
+            graph[i][j] = 1
 
 dx, dy = [1,-1,0,0], [0,0,1,-1]
 
@@ -18,37 +18,41 @@ counts = []
 def bfs(x,y):
     global count
     queue = deque([(x,y)])
-    graph[y][x] = 1
+    graph[x][y] = 1
 
     while queue:
-        nx, ny = queue.popleft()
+        x, y = queue.popleft()
     
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx >= m or ny >= n or nx < 0 or ny < 0:
+            if nx >= n or ny >= m or nx < 0 or ny < 0:
                 continue
 
             # 벽일 경우
-            if graph[ny][nx] == -1:
+            if graph[nx][ny] == -1:
                 continue
             
             # 방문 안한 경우
-            if graph[ny][nx] == 0:
+            if graph[nx][ny] == 0:
                 queue.append((nx, ny))
-                graph[ny][nx] = 1
+                graph[nx][ny] = 1
                 count += 1
-                print(count)
+    
 
+count = 0
 
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0:
+            count = 1
+            bfs(i, j)
+            counts.append(count)
+            
+counts.sort()
 
-for i in range(m):
-    for j in range(n):
-        print(i,j)
-        count = 0
-        bfs(i, j)
-        print(graph)
-        counts.append(count)
+print(len(counts))
 
-print(counts)
+for count in counts:
+    print(count)
