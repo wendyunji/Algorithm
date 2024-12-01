31da7e3b75dd3c7474d6287a5a3ddfe05d87241b
Yunji <wendyunji@gmail.com>
2024-12-02 08:11:54 +0900

    clear

---
+++ bs/[BOJ]_10815.py
@@ -0,0 +1,29 @@
+import sys
+
+n = int(input())
+array = list(map(int, sys.stdin.readline().rstrip().split()))
+
+m = int(input())
+targets = list(map(int, sys.stdin.readline().rstrip().split()))
+
+array.sort()
+
+def bs(array, target, start, end):
+
+    while start <= end:
+        
+        mid = (start+end) // 2
+
+        if array[mid] == target:
+            return 1
+        
+        elif array[mid] > target:
+            end = mid - 1
+
+        else:
+            start = mid + 1
+
+    return 0
+
+for target in targets:
+    print(bs(array, target, 0, n-1), end=' ')
