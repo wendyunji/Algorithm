494445c5e05921ae40453cd561ce2a732ce70237
Yunji <wendyunji@gmail.com>
2024-12-07 16:08:55 +0900

    solved
    need retry

--- bs/[BOJ]_2512.py
+++ bs/[BOJ]_2512.py
@@ -4,42 +4,23 @@ n = int(input())
 array = list(map(int, sys.stdin.readline().rstrip().split()))
 m = int(input())
 
-cost = 0
-basket = []
-result = 0
+start = 1
+end = max(array)
 
-def div(array, m):
-    global result
-    left_cost = 0
-    n = len(array)
-    avg = m // n
-
-    left = []
+while start <= end:
+    mid = (start+end) // 2
+    cost = 0
     for a in array:
-        if a <= avg:
-            left_cost += a
-            basket.append(a)
+        if a > mid:
+            cost += mid
         else:
-            left.append(a)
-    m = m - left_cost
+            cost += a
+    if cost <= m:
+        start = mid + 1
+    else:
+        end = mid - 1
 
-    cnt = 0
-    for b in left:
-        if b > avg:
-            cnt += 1
-    if cnt == len(left):
-        result = avg
-
-    return left, basket, left_cost, m
-
-if sum(array) <= m:
-    result = max(array)
-else:
-    while cost <= m:
-        array, basket, left_cost, m = div(array, m)
-        cost += left_cost
-        if left_cost == 0:
-            break
+print(end)
 
 
-print(result)
+   
