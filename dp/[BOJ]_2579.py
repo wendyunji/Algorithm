8524ded088b18d5c198990c1ba694ea06322025f
Yunji <wendyunji@gmail.com>
2024-12-18 15:28:36 +0900

    solved

--- dp/[BOJ]_2579.py
+++ dp/[BOJ]_2579.py
@@ -1,14 +1,16 @@
 n = int(input())
 
-array = [0]
-d = [0]*(n+1)
-for _ in range(n):
-    array.append(int(input()))
+array = [0]*(n+3)
+d = [0]*(n+3)
+for i in range(1,n+1):
+    array[i] = int(input())
 
-for i in range(0,n+1):
-    d[i] = d[i-1] + array[i]
+d[1] = array[1]
+d[2] = array[1] + array[2]
+d[3] = max(array[1]+array[3], array[2]+array[3])
 
-    if d[i-2] > d[i-2]:
 
+for i in range(4,n+1):
+    d[i] = max(d[i-3]+array[i-1]+array[i], d[i-2]+array[i])
 
-print(d)
+print(d[n])
