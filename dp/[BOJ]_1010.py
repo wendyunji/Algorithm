099c3413d73038a8847bdcd196dbf512321cefbf
Yunji <wendyunji@gmail.com>
2024-12-25 23:58:12 +0900

    solved

---
+++ dp/[BOJ]_1010.py
@@ -0,0 +1,22 @@
+print(67863915/29)
+print(2340135/5)
+print(468027/3)
+print(156009/3)
+print(52003/7)
+print(7429/17)
+print(437/19)
+
+cnt1 = 1
+for i in range(17,30):
+    print(i)
+    cnt1 *= i
+
+print(cnt1)
+
+cnt = 1
+for i in range(1,14):
+    print(i)
+    cnt *= i
+
+print(cnt)
+print(cnt1/cnt)
